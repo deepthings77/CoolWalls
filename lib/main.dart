cbdfed206275294bbc11f4acbaa58f38163d20bb
Deepak <kumawatdeepak1221@gmail.com>
2023-09-14 19:03:38 +0530

    final3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
+// import 'package:flutter/material.dart';
+// import 'package:my_new/views/Screen/home.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'CoolWalls',
+//       theme: ThemeData(primarySwatch: Colors.red),
+//       home: const HomeScreen(),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:my_new/views/Screen/home.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +31,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Request the permission as soon as the app starts
+    _requestPermission();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'CoolWalls',
       theme: ThemeData(primarySwatch: Colors.red),
       home: const HomeScreen(),
     );
+  }
+
+  // Function to request the WRITE_EXTERNAL_STORAGE permission
+  Future<void> _requestPermission() async {
+    // Request the WRITE_EXTERNAL_STORAGE permission
+    var status = await Permission.storage.request();
+
+    if (status.isGranted) {
+      // Permission is granted
+      print('Permission is granted');
+    } else if (status.isDenied) {
+      // Permission is denied
+      print('Permission is denied');
+    } else if (status.isPermanentlyDenied) {
+      // Permission is permanently denied
+      print('Permission is permanently denied');
+    }
   }
 }
--- lib/views/widgets/search_bar.dart
+++ lib/views/widgets/search_bar.dart
@@ -1,3 +1,48 @@
+// import 'package:flutter/material.dart';
+// import 'package:my_new/views/Screen/search.dart';
+
+// class SearchBarThing extends StatelessWidget {
+//   SearchBarThing({super.key});
+
+//   final TextEditingController _searchController = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         padding: const EdgeInsets.symmetric(horizontal: 20),
+//         decoration: BoxDecoration(
+//             color: const Color.fromARGB(66, 80, 80, 80),
+//             border: Border.all(color: const Color.fromARGB(32, 0, 0, 0)),
+//             borderRadius: BorderRadius.circular(25)),
+//         child: Row(
+//           children: [
+//             Expanded(
+//               child: TextField(
+//                 controller: _searchController,
+//                 decoration: const InputDecoration(
+//                   hintText: "Search Wallpapers",
+//                   errorBorder: InputBorder.none,
+//                   focusedBorder: InputBorder.none,
+//                   focusedErrorBorder: InputBorder.none,
+//                   disabledBorder: InputBorder.none,
+//                   enabledBorder: InputBorder.none,
+//                   border: InputBorder.none,
+//                 ),
+//               ),
+//             ),
+//             InkWell(
+//                 onTap: () {
+//                   Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                           builder: (context) =>
+//                               SearchScreen(query: _searchController.text)));
+//                 },
+//                 child: const Icon(Icons.search))
+//           ],
+//         ));
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:my_new/views/Screen/search.dart';
 
@@ -6,40 +51,54 @@ class SearchBarThing extends StatelessWidget {
 
   final TextEditingController _searchController = TextEditingController();
 
+  void _performSearch(BuildContext context) {
+    final query = _searchController.text;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SearchScreen(query: query),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        decoration: BoxDecoration(
-            color: const Color.fromARGB(66, 80, 80, 80),
-            border: Border.all(color: const Color.fromARGB(32, 0, 0, 0)),
-            borderRadius: BorderRadius.circular(25)),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: _searchController,
-                decoration: const InputDecoration(
-                  hintText: "Search Wallpapers",
-                  errorBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  focusedErrorBorder: InputBorder.none,
-                  disabledBorder: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  border: InputBorder.none,
-                ),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(66, 80, 80, 80),
+        border: Border.all(color: const Color.fromARGB(32, 0, 0, 0)),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _searchController,
+              onSubmitted: (_) {
+                // Trigger search when "Enter" key is pressed
+                _performSearch(context);
+              },
+              decoration: const InputDecoration(
+                hintText: "Search Wallpapers",
+                errorBorder: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                focusedErrorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                border: InputBorder.none,
               ),
             ),
-            InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>
-                              SearchScreen(query: _searchController.text)));
-                },
-                child: const Icon(Icons.search))
-          ],
-        ));
+          ),
+          InkWell(
+            onTap: () {
+              // Trigger search when search icon is tapped
+              _performSearch(context);
+            },
+            child: const Icon(Icons.search),
+          )
+        ],
+      ),
+    );
   }
 }
